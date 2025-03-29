9d9bd5c4a0e46d5d7141245348776fdb62bd0121
Rohitparakh <rohitparakh4@gmail.com>
2025-03-29 16:53:23 +0530

    removed comments

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,8 +1,6 @@
 import mongoose, { Model, model, Schema } from "mongoose";
 
 export interface UserData {
-  // userName: string;
-  // userId: string;
   pubKey: string;
   prvKey: string;
   solBalance: number;
@@ -11,8 +9,6 @@ export interface UserData {
 interface UserDataModel extends Model<UserData> {}
 
 const UserDataSchema: Schema<UserData> = new mongoose.Schema({
-  // userName: { type: String, required: true },
-  // userId: { type: String, required: true },
   pubKey: { type: String, required: true },
   prvKey: { type: String, required: true },
   solBalance: { type: Number, required: true, default: 50 },
