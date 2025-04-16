e85b315f7d69b8eecda74a06741acc0701009ec4
Rohitparakh <rohitparakh4@gmail.com>
2025-04-16 13:56:52 +0530

    added discordroute to trade

--- src/routes/trade.router.ts
+++ src/routes/trade.router.ts
@@ -2,6 +2,7 @@ import express  from "express";
 import { trade, walletTokens, priceFetchinUSD } from "../controller/trade.controller";
 import { fetchBondingCurve } from "../utils/pump";
 import { fetchTokenData } from "../utils/pump";
+import { discordLogin } from "../controller/discord.controller";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.post("/wallet", walletTokens);
 router.get("/price/:mint",priceFetchinUSD);
 router.get("/raydium/:mint", fetchBondingCurve);
 router.get("/tokenData/:mint", fetchTokenData);
+router.get("/auth/discord/callback",discordLogin);
 
 export default router;
